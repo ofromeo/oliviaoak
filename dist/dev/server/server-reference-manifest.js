da3f6d86d715c6f483a127b9e72c5cdfe8ed12d3
unknown <ofromeosantos@gmail.com>
2026-07-20 12:56:01 -0700

    Update TikTok URL 2

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"50YHrkfcJfiNzEhYtfU9BcIy/Co3uCP4B7Ed3W8Qsg4=\"\n}"
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ['172.21.160.1'],
-    output: 'export',
-    distDir: 'dist',
-    images: {
-        unoptimized: true,
-    },
+    
     
 }
 
